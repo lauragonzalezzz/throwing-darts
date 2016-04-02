c66aae4d1b70c48f7c1beac358ea99959d8fa439
Laura <lg3295@gmail.com>
2016-04-02 09:28:01 -1000

    initial commit

---
+++ public/js/tests/index.spec.js
@@ -0,0 +1,6 @@
+describe('Score Throws', function(){
+
+  it('should exist', function(){
+    expect(scoreThrows).to.be.a('function');
+  });
+});
---
+++ test/index.spec.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var vm = require('vm');
+var chai = require('chai');
+var functionsFile = fs.readFileSync(process.cwd() + '/public/js/app.js');
+vm.runInThisContext(functionsFile); // file runs and it's contents has access to GLOBAL
+
+var expect = chai.expect;
+var should = chai.should();
+
+describe('function exists', function(){
+  it('should exist', function(){
+    expect(5).to.equal(5);
+  })
+})
