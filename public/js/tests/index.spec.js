8c6f534e47965a4675292a7574a5363df923a77d
Laura <lg3295@gmail.com>
2016-04-02 09:58:13 -1000

    tests that the proper score is returned, that a bonus is added, and that it returns zero if no scores are provided

--- public/js/tests/index.spec.js
+++ public/js/tests/index.spec.js
@@ -5,6 +5,18 @@ describe('Score Throws', function(){
   });
 
   it('should return a number', function(){
-    expect(scoreThrows()).to.be.a('number');
+    expect(scoreThrows([3,4,5])).to.be.a('number');
+  });
+
+  it('should return a score based on input array', function(){
+    expect(scoreThrows([4,3,2,7])).to.equal(35);
+  });
+
+  it('should award a bonus of 100 if each radius is less than 5', function(){
+    expect(scoreThrows([4,3,2,3])).to.equal(140);
+  });
+
+  it('should return a score of zero on an empty array', function(){
+    expect(scoreThrows([])).to.equal(0);
   });
 });
