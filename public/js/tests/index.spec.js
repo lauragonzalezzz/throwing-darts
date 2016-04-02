1e3007a38a18bcd08a8e654a96b4900485c62f16
Laura <lg3295@gmail.com>
2016-04-02 09:39:18 -1000

    tests that scoreThrows returns a number

--- public/js/tests/index.spec.js
+++ public/js/tests/index.spec.js
@@ -3,4 +3,8 @@ describe('Score Throws', function(){
   it('should exist', function(){
     expect(scoreThrows).to.be.a('function');
   });
+
+  it('should return a number', function(){
+    expect(scoreThrows()).to.be.a('number');
+  });
 });
