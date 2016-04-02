ba8811b64c4068639ef85615e4ec70401b953952
Laura <lg3295@gmail.com>
2016-04-02 09:39:01 -1000

    maps scores array to points

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
-function scoreThrows(){};
+function scoreThrows(arr){
+  var bonusCalcArray = arr.map(function(scores){
+    // console.log('scores',scores);
+    if (scores > 10){
+      scores = 0;
+    }
+    else if (scores <= 10 && scores >= 5){
+      scores = 5;
+    }
+    else {
+      scores = 10;
+    };
+    console.log('scores',scores);
+    return scores;
+  });
+
+
+
+}; //End of scoreThrows
+
+scoreThrows([3,6,8]);
